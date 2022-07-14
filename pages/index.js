fe690c91922399caab829557d38f034f423e6f12
daxtynh <daxtynosburn@gmail.com>
2022-07-13 20:07:22 -0500

    everything

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { Hero } from '@components/Hero'
+import { About } from '@components/About'
+import { Portfolio } from '@components/Portfolio'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Home Page!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header title="Welcome to my app!" />
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Hero />
+        <Portfolio />
+        <About />
       </main>
+      
 
       <Footer />
     </div>
